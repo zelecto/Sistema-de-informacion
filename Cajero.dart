c07a35e92b4da46bc69d882fb4b2190b49f4246f
zelecto <ca346364@gmail.com>
2024-08-16 10:28:11 -0500

    Culminacion

---
+++ Cajero.dart
@@ -0,0 +1,92 @@
+import 'dart:io';
+
+const List<int> billetes = [10, 20, 50, 100];
+
+void main(List<String> args) {
+  print('digite el valor a retirar: ');
+  String? captura = stdin.readLineSync();
+  print('Metodo normal :' +
+      Formateo(Despacho(int.tryParse(captura ?? '0') ?? 0)).toString());
+  print('digite el valor a retirar: ');
+  captura = stdin.readLineSync();
+  print('Metodo Comdin :' + ComodinDespacho(150).toString());
+}
+
+List<List<int>> Despacho(int solicitado) {
+  int monto = 0;
+  int filas = 0;
+  List<List<int>> resultado = [];
+  List<int> acarreo = [];
+  while (monto < solicitado) {
+    resultado.add([]);
+    for (var i = 0; i < 4; i++) {
+      if (i + 1 <= acarreo.length && acarreo[i] == 1) {
+        resultado[filas].add(0);
+      } else if (monto + billetes[i] <= solicitado) {
+        resultado[filas].add(billetes[i]);
+        monto += billetes[i];
+      }
+    }
+    acarreo.add(1);
+    filas++;
+  }
+
+  return resultado;
+}
+
+Map<String, int> Formateo(List<List<int>> listaValores) {
+  Map<String, int> numeroBilletes = {
+    "10k": 0,
+    "20k": 0,
+    "50k": 0,
+    "100k": 0,
+  };
+  for (var i = 0; i < listaValores.length; i++) {
+    for (var j = 0; j < listaValores[i].length; j++) {
+      switch (listaValores[i][j]) {
+        case 10:
+          numeroBilletes["10k"] = 1 + (numeroBilletes["10k"] ?? 0);
+          break;
+        case 20:
+          numeroBilletes["20k"] = 1 + (numeroBilletes["20k"] ?? 0);
+          break;
+        case 50:
+          numeroBilletes["50k"] = 1 + (numeroBilletes["50k"] ?? 0);
+          break;
+        case 100:
+          numeroBilletes["100k"] = 1 + (numeroBilletes["100k"] ?? 0);
+          break;
+
+        default:
+      }
+    }
+  }
+
+  return numeroBilletes;
+}
+
+Map<String, int> ComodinDespacho(int solicitado) {
+  int monto = 0;
+  Map<String, int> resultados = {
+    "10k": 0,
+    "20k": 0,
+    "50k": 0,
+    "100k": 0,
+  };
+  while (monto < solicitado) {
+    for (var i = 3; i >= 0; i--) {
+      if (monto + billetes[i] <= solicitado) {
+        var numeroBilletes = descomponer(solicitado - monto, billetes[i]);
+        resultados[billetes[i].toString() + 'k'] =
+            numeroBilletes + (resultados[billetes[i].toString() + 'k'] ?? 0);
+        monto += numeroBilletes * billetes[i];
+      }
+    }
+  }
+
+  return resultados;
+}
+
+int descomponer(int valor, int valorDeBillete) {
+  return valor ~/ valorDeBillete;
+}
