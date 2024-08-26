5ee106980c20245b9f79f0301bbf556970af3031
zelecto <ca346364@gmail.com>
2024-08-26 17:07:48 -0500

    mejoras generales y adaciones de nuevas variables para una mejor validacion y visibilidad

--- Cajero/cajero/lib/presetation/screens/register/widget/text_field_from.dart
+++ Cajero/cajero/lib/presetation/screens/register/widget/text_field_from.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TextFieldFrom extends StatelessWidget {
   const TextFieldFrom({
@@ -7,29 +8,37 @@ class TextFieldFrom extends StatelessWidget {
     required this.labelText,
     required this.hintText,
     this.maxLength,
+    this.minLength,
     this.type,
+    this.inputFormatters = const [],
+    this.validator,
   });
 
   final TextEditingController controller;
   final String labelText;
   final String hintText;
   final int? maxLength;
+  final int? minLength;
   final TextInputType? type;
+  final List<TextInputFormatter> inputFormatters;
+  final String? Function(String?)? validator;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(5),
-      child: TextField(
+      padding: const EdgeInsets.all(10),
+      child: TextFormField(
         keyboardType: type,
         controller: controller,
         maxLength: maxLength,
+        inputFormatters: inputFormatters,
+        validator: validator,
         decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-          labelText: labelText,
-          hintText: hintText,
-          hintStyle: const TextStyle(color: Colors.grey),
-        ),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+            labelText: labelText,
+            hintText: hintText,
+            hintStyle: const TextStyle(color: Colors.grey),
+            counterText: ''),
       ),
     );
   }
