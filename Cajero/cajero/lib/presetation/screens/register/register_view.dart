5a5b05e5ebd3b62c92fa0627de1c1308243e8e04
zelecto <ca346364@gmail.com>
2024-08-26 17:06:18 -0500

    mejoras visuales generales

--- Cajero/cajero/lib/presetation/screens/register/register_view.dart
+++ Cajero/cajero/lib/presetation/screens/register/register_view.dart
@@ -1,7 +1,7 @@
 import 'package:cajero/config/tools/color_util.dart';
 import 'package:cajero/config/tools/screen_size.dart';
-import 'package:cajero/presetation/screens/register/widget/text_field_from.dart';
-import 'package:cajero/presetation/screens/register/widget/update_colors.dart';
+import 'package:cajero/presetation/screens/register/acount_nequi_from.dart';
+import 'package:cajero/presetation/screens/register/credt_card_form.dart';
 import 'package:flutter/material.dart';
 import 'package:awesome_card/awesome_card.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -69,7 +69,7 @@ class RegisterView extends HookWidget {
                     textExpDate: 'Exp. Date',
                     textExpiry: 'MM/YY',
                   ),
-                  _CreditCardForm(
+                  CreditCardForm(
                     numberCard: numberCard,
                     selectedColor: selectedColor,
                     securityCode: securityCode,
@@ -91,195 +91,17 @@ class RegisterView extends HookWidget {
                     'Reguistrar Cuenta Nequi',
                     style: textStyle,
                   ),
-                  Image.network(
-                      'https://i.pinimg.com/564x/c9/9d/14/c99d1437635da2d96561a8e37f0d4d4e.jpg'),
-                  const _AcountNequiFrom()
+                  Image.asset(
+                    'assets/images/nequi.png',
+                    width: ScreenSize.getWidth(context) * 0.8,
+                  ),
+                  const AcountNequiFrom()
                 ],
               ),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class _CreditCardForm extends HookWidget {
-  const _CreditCardForm(
-      {required this.numberCard,
-      required this.selectedColor,
-      required this.holderName,
-      required this.expirationDate,
-      required this.securityCode,
-      required this.showBackSide});
-
-  final ValueNotifier<String> numberCard;
-  final ValueNotifier<String> holderName;
-  final ValueNotifier<String> expirationDate;
-  final ValueNotifier<String> securityCode;
-  final ValueNotifier<Color> selectedColor;
-  final ValueNotifier<bool> showBackSide;
-
-  @override
-  Widget build(BuildContext context) {
-    final controllerNumberCard = useTextEditingController();
-    final controllerHolderName = useTextEditingController();
-    final controllerExpirationDate = useTextEditingController();
-    final controllerCcv = useTextEditingController();
-
-    // Listener para el número de la tarjeta
-    useEffect(() {
-      void listener() {
-        showBackSide.value ? showBackSide.value = false : null;
-        final rawText =
-            controllerNumberCard.text.replaceAll(RegExp(r'\s+'), '');
-        final formattedText = rawText
-            .replaceAllMapped(RegExp(r'.{4}'), (match) => '${match.group(0)} ')
-            .trim();
-
-        if (numberCard.value != formattedText) {
-          numberCard.value = formattedText;
-        }
-      }
-
-      controllerNumberCard.addListener(listener);
-      return () => controllerNumberCard.removeListener(listener);
-    }, [controllerNumberCard]);
-
-    // Sincronizar el controlador con el ValueNotifier
-    useEffect(() {
-      controllerNumberCard.text = numberCard.value;
-      controllerNumberCard.selection = TextSelection.fromPosition(
-        TextPosition(offset: controllerNumberCard.text.length),
-      );
-      return null;
-    }, [numberCard.value]);
-
-    // Listener para el nombre del titular
-    useEffect(() {
-      void listener() {
-        showBackSide.value ? showBackSide.value = false : null;
-        if (holderName.value != controllerHolderName.text) {
-          holderName.value = controllerHolderName.text;
-        }
-      }
-
-      controllerHolderName.addListener(listener);
-      return () => controllerHolderName.removeListener(listener);
-    }, [controllerHolderName]);
-
-    // Listener para la fecha de vencimiento
-    useEffect(() {
-      void listener() {
-        showBackSide.value ? showBackSide.value = false : null;
-        if (expirationDate.value != controllerExpirationDate.text) {
-          expirationDate.value = controllerExpirationDate.text;
-        }
-      }
-
-      controllerExpirationDate.addListener(listener);
-      return () => controllerExpirationDate.removeListener(listener);
-    }, [controllerExpirationDate]);
-
-    // Listener para el código de seguridad
-    useEffect(() {
-      void listener() {
-        showBackSide.value = true;
-        if (securityCode.value != controllerCcv.text) {
-          securityCode.value = controllerCcv.text;
-          controllerCcv.text.length == 4
-              ? Future.delayed(const Duration(seconds: 2), () {
-                  showBackSide.value = false;
-                })
-              : null;
-        }
-      }
-
-      controllerCcv.addListener(listener);
-      return () => controllerCcv.removeListener(listener);
-    }, [controllerCcv]);
-
-    return Column(
-      children: [
-        UpdateColors(selectedColor: selectedColor),
-        TextFieldFrom(
-          controller: controllerNumberCard,
-          hintText: '0000 0000 0000 0000',
-          labelText: 'Número de la tarjeta',
-          maxLength: 19,
-          type: TextInputType.number,
-        ),
-        TextFieldFrom(
-          controller: controllerHolderName,
-          labelText: 'Titular de la tarjeta',
-          maxLength: 20,
-          hintText: 'Nombre',
-        ),
-        Row(
-          children: [
-            SizedBox(
-              width: ScreenSize.getWidth(context) * 0.4,
-              child: TextFieldFrom(
-                controller: controllerExpirationDate,
-                maxLength: 5, // Para formato 'MM/YY'
-                labelText: 'Vencimiento',
-                hintText: '00/00',
-                type: TextInputType.number,
-              ),
-            ),
-            const Spacer(),
-            SizedBox(
-              width: ScreenSize.getWidth(context) * 0.4,
-              child: TextFieldFrom(
-                controller: controllerCcv,
-                maxLength: 4,
-                labelText: 'CCV',
-                hintText: '000',
-                type: TextInputType.number,
-              ),
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: SizedBox(
-            width: ScreenSize.getWidth(context) * 0.4,
-            child: FilledButton(
-              onPressed: () {},
-              style: FilledButton.styleFrom(backgroundColor: Colors.green),
-              child: const Text('Guardar'),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class _AcountNequiFrom extends HookWidget {
-  const _AcountNequiFrom();
-
-  @override
-  Widget build(BuildContext context) {
-    final controllerName = useTextEditingController();
-    return Column(
-      children: [
-        TextFieldFrom(
-            controller: controllerName,
-            labelText: 'Nombre',
-            hintText: 'Carlos'),
-        TextFieldFrom(
-            controller: controllerName,
-            labelText: 'Telefono',
-            hintText: '0000000000'),
-        TextFieldFrom(
-            controller: controllerName,
-            labelText: 'cedula',
-            hintText: 'Carlos'),
-        SizedBox(
-          height: ScreenSize.getHeight(context) * 0.15,
-        )
-      ],
     );
   }
 }
