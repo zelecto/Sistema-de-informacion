e6d085caa564fbe6e6ce21dbbe781bfc51ff805f
zelecto <ca346364@gmail.com>
2024-08-30 16:36:17 -0500

    implementacion de logica y enlazamiento de vistas

--- Cajero/cajero/lib/presetation/screens/retirar/credit_card/retirar_credit_card_home_view.dart
+++ Cajero/cajero/lib/presetation/screens/retirar/credit_card/retirar_credit_card_home_view.dart
@@ -5,11 +5,13 @@ import 'package:cajero/config/tools/color_util.dart';
 import 'package:cajero/config/tools/screen_size.dart';
 import 'package:cajero/domain/entity/credit_card.dart';
 import 'package:cajero/domain/infrastructure/credit_card_data.dart';
+import 'package:cajero/presetation/provider/credit_cart/credit_cart_provaider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class RetirarCreditCard extends HookWidget {
+class RetirarCreditCard extends HookConsumerWidget {
   const RetirarCreditCard({super.key});
   static const String name = 'retirar-credit-card';
   final textStyle = const TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
@@ -19,9 +21,10 @@ class RetirarCreditCard extends HookWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var showBackSide = useState(false);
     var draggedCard = useState<CreditCardEntity?>(null);
+    var enableButton = useState(false);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -37,6 +40,7 @@ class RetirarCreditCard extends HookWidget {
             DragTarget<CreditCardEntity>(
               onAccept: (data) {
                 draggedCard.value = data;
+                enableButton.value = true;
               },
               builder: (context, candidateData, rejectedData) {
                 return Container(
@@ -173,9 +177,13 @@ class RetirarCreditCard extends HookWidget {
               width: double.infinity,
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: FilledButton(
-                onPressed: () {
-                  context.go('/monto_selecionar');
-                },
+                onPressed: enableButton.value
+                    ? () {
+                        ref.read(creditCardProvider.notifier).state =
+                            draggedCard.value!;
+                        context.go('/monto_selecionar');
+                      }
+                    : null,
                 child: const Text("Continuar"),
               ),
             )
