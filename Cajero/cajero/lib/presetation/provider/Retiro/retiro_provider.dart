fa9386da34cc5c4297638d1889324d68a91418ff
zelecto <ca346364@gmail.com>
2024-08-30 16:36:39 -0500

    actualizacion de gestion del estado para un mejor funcionamiento

--- Cajero/cajero/lib/presetation/provider/Retiro/retiro_provider.dart
+++ Cajero/cajero/lib/presetation/provider/Retiro/retiro_provider.dart
@@ -1,8 +1,4 @@
 import 'package:cajero/domain/entity/retiro.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-part 'retiro_provider.g.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-@riverpod
-Retiro retiroProvider(RetiroProviderRef ref, Retiro retiro) {
-  return retiro;
-}
+final retiroProvider = StateProvider<Retiro?>((ref) => null);
