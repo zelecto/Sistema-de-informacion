487e5876b994df9ade18535bb56fcfc14f6871de
zelecto <ca346364@gmail.com>
2024-08-30 09:12:47 -0500

    implementacon de rutas para las nuevas vistas

--- Cajero/cajero/lib/config/router/app_router.dart
+++ Cajero/cajero/lib/config/router/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:cajero/presetation/screens/home/cajero_home_view.dart';
 import 'package:cajero/presetation/screens/register/register_view.dart';
 import 'package:cajero/presetation/screens/retirar/credit_card/retirar_credit_card_home_view.dart';
+import 'package:cajero/presetation/screens/retirar/credit_card/retirar_credit_card_password.dart';
+import 'package:cajero/presetation/screens/retirar/widget/monto_selecionar_view.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
+final appRouter = GoRouter(initialLocation: '/monto_selecionar', routes: [
   GoRoute(
     path: '/',
     name: CajeroHomeView.name,
@@ -18,5 +20,15 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
     path: '/retirar_credit_card',
     name: RetirarCreditCard.name,
     builder: (context, state) => const RetirarCreditCard(),
+  ),
+  GoRoute(
+    path: '/retirar_credit_card_password',
+    name: RetirarCreditCardPassword.name,
+    builder: (context, state) => const RetirarCreditCardPassword(),
+  ),
+  GoRoute(
+    path: '/monto_selecionar',
+    name: MontoSelecionarView.name,
+    builder: (context, state) => const MontoSelecionarView(),
   ),
 ]);
