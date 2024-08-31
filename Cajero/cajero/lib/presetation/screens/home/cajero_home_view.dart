e66aa959b5f244083b11c55689704d2915840a6d
zelecto <ca346364@gmail.com>
2024-08-31 17:49:53 -0500

    mejoras UI

--- Cajero/cajero/lib/presetation/screens/home/cajero_home_view.dart
+++ Cajero/cajero/lib/presetation/screens/home/cajero_home_view.dart
@@ -9,111 +9,89 @@ class CajeroHomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Scaffold(
-        appBar: AppBar(
-          actions: [
-            Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: FilledButton(
-                    onPressed: () {
-                      context.push('/register');
-                    },
-                    child: const Text("Reguistrarse")))
-          ],
-          title: Row(
+      body: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Image.asset(
-                'assets/images/Icono_Bancolombia.png',
-                width: ScreenSize.getWidth(context) * 0.1,
-              ),
-              const SizedBox(
-                width: 10,
+                'assets/images/logo.png',
+                width: ScreenSize.getWidth(context),
+                height: ScreenSize.getHeight(context) * 0.2,
+                fit: BoxFit.cover,
               ),
               const Text(
-                "Bancolombia",
-                style: TextStyle(fontWeight: FontWeight.bold),
+                "Bienvenido(a)! Por favor, elige una de las siguientes opciones",
+                softWrap: true,
+                maxLines: null,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              _CustomButton(
+                img: 'assets/images/logo_nequi.png',
+                onPressed: () {},
+                textButton: 'retiros nequi',
+              ),
+              _CustomButton(
+                onPressed: () {
+                  context.push('/retirar_credit_card');
+                },
+                img: 'assets/images/credit_card.png',
+                textButton: 'retiros tarjeta',
+              ),
+              _CustomButton(
+                onPressed: () {
+                  context.push('/register');
+                },
+                img: 'assets/images/registrarse.jpeg',
+                textButton: 'registrarse',
               ),
             ],
           ),
         ),
-        body: SizedBox(
-          width: double.infinity,
+      ),
+    );
+  }
+}
+
+class _CustomButton extends StatelessWidget {
+  final String textButton;
+  final String img;
+  final onPressed;
+  const _CustomButton({
+    required this.textButton,
+    required this.img,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: SizedBox(
+        width: ScreenSize.getHeight(context) * 0.2,
+        height: ScreenSize.getHeight(context) * 0.15,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Color(Colors.white.value),
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)))),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/home.png',
-                    width: ScreenSize.getWidth(context) * 0.4,
-                    height: ScreenSize.getHeight(context) * 0.3,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  SizedBox(
-                    width: ScreenSize.getWidth(context) * 0.5,
-                    child: const Text(
-                      "Bienvenido(a)! Por favor, elige una de las siguientes opciones",
-                      softWrap: true,
-                      maxLines: null,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                  ),
-                ],
+              Image.asset(
+                img,
+                width: ScreenSize.getWidth(context) * 0.25,
+                fit: BoxFit.cover,
               ),
-              SizedBox(
-                height: ScreenSize.getHeight(context) * 0.15,
+              const SizedBox(
+                width: 5,
               ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: ScreenSize.getWidth(context) * 0.4,
-                    height: ScreenSize.getHeight(context) * 0.05,
-                    child: TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5)))),
-                      child: const Row(
-                        children: [
-                          Icon(Icons.arrow_back_ios_new_rounded),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text("Retiros Nequi")
-                        ],
-                      ),
-                    ),
-                  ),
-                  const Spacer(),
-                  SizedBox(
-                    width: ScreenSize.getWidth(context) * 0.4,
-                    height: ScreenSize.getHeight(context) * 0.05,
-                    child: TextButton(
-                      onPressed: () {
-                        context.push('/retirar_credit_card');
-                      },
-                      style: TextButton.styleFrom(
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5)))),
-                      child: const Row(
-                        children: [
-                          Spacer(),
-                          Text("Retiro Tarjeta"),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Icon(Icons.arrow_forward_ios_rounded),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+              Text(
+                textButton,
+                style: const TextStyle(fontSize: 20),
               )
             ],
           ),
