b8a892ff4d9bc290360e785673584d1e634eda47
zelecto <ca346364@gmail.com>
2024-09-01 18:39:50 -0500

    arreglo de bug, se estaba realizando una validacion mal

--- Cajero/cajero/lib/presetation/screens/retirar/credit_card/retirar_credit_card_password.dart
+++ Cajero/cajero/lib/presetation/screens/retirar/credit_card/retirar_credit_card_password.dart
@@ -1,42 +1,88 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:awesome_card/credit_card.dart';
+import 'package:cajero/config/tools/screen_size.dart';
+import 'package:cajero/presetation/provider/credit_cart/credit_cart_provaider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class RetirarCreditCardPassword extends HookWidget {
+class RetirarCreditCardPassword extends HookConsumerWidget {
   const RetirarCreditCardPassword({super.key});
   static const String name = 'retirar-credit-card-password121321';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final controllerCcv = useTextEditingController();
+    final creditCard = ref.read(creditCardProvider);
     final focusNode = useFocusNode();
+    final numIntentosDisponibles = useState(3);
+
+    void validarCcv() {
+      if (controllerCcv.text.length == 3 &&
+          creditCard!.cvv == controllerCcv.text) {
+      } else if (controllerCcv.text.length == 3) {
+        numIntentosDisponibles.value--;
+        if (numIntentosDisponibles.value <= 0) {
+          _showDialog(
+              context, 'Se han agotado los intentos, Se bloqueara la cuenta');
+        } else {
+          _showSnackBar(context,
+              'Código incorrecto. Intentos restantes: ${numIntentosDisponibles.value}');
+          controllerCcv.clear();
+        }
+      }
+    }
 
     useEffect(() {
       WidgetsBinding.instance.addPostFrameCallback((_) {
         FocusScope.of(context).requestFocus(focusNode);
       });
 
-      focusNode.addListener(() {
-        if (!focusNode.hasFocus) {
-          FocusScope.of(context).requestFocus(focusNode);
+      controllerCcv.addListener(() {
+        if (controllerCcv.text.length == 3) {
+          validarCcv();
         }
       });
 
       return () {
         focusNode.dispose();
+        controllerCcv.dispose();
       };
-    }, [focusNode]);
+    }, []);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Digite su código CCV'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/Icono_Bancolombia.png',
+              width: 30,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            const Text(
+              'Bancolombia',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ],
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Image.asset(
+              'assets/images/candado.png',
+              height: ScreenSize.getHeight(context) * 0.3,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
             TextField(
               controller: controllerCcv,
               maxLength: 3,
@@ -51,16 +97,76 @@ class RetirarCreditCardPassword extends HookWidget {
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               focusNode: focusNode,
             ),
+            // ignore: unrelated_type_equality_checks
+            if (numIntentosDisponibles.value == 1)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                child: FadeIn(
+                  child: const Text(
+                    'Solo te queda un intento. Si fallas, tu cuenta será bloqueada.',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.red),
+                  ),
+                ),
+              ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                context.go('/recibo');
-              },
-              child: const Text('Aceptar'),
-            ),
           ],
         ),
       ),
     );
   }
+}
+
+void _showDialog(BuildContext context, String message) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: SizedBox(
+          height: ScreenSize.getHeight(context) * 0.2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/images/error.png',
+                height: ScreenSize.getHeight(context) * 0.1,
+                width: ScreenSize.getWidth(context) * 0.5,
+                fit: BoxFit.cover,
+              ),
+              Text(message,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  )),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              //context.go('/');
+              Navigator.of(context).pop();
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+void _showSnackBar(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: Color(Colors.red.value),
+      content: Text(
+        message,
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+      ),
+      duration: const Duration(seconds: 2),
+    ),
+  );
 }
